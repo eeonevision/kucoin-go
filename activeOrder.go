7f9fd6cffd49a1a5ff499442003a272c93a0ade9
Vladislav Dmitriyev <d7vladislav@gmail.com>
2018-05-02 16:32:27 +0300

    Fixed ActiveOrder struct

--- activeOrder.go
+++ activeOrder.go
@@ -1,13 +1,38 @@
 package kucoin
 
-type ActiveOrders struct {
-	SELL []interface{}   `json:"SELL"`
-	BUY  [][]interface{} `json:"BUY"`
+type ActiveOrder struct {
+	SELL []struct {
+		Oid           string      `json:"oid"`
+		Type          string      `json:"type"`
+		UserOid       interface{} `json:"userOid"`
+		CoinType      string      `json:"coinType"`
+		CoinTypePair  string      `json:"coinTypePair"`
+		Direction     string      `json:"direction"`
+		Price         float64     `json:"price"`
+		DealAmount    float64     `json:"dealAmount"`
+		PendingAmount float64     `json:"pendingAmount"`
+		CreatedAt     int64       `json:"createdAt"`
+		UpdatedAt     int64       `json:"updatedAt"`
+	} `json:"SELL"`
+	BUY []struct {
+		Oid           string      `json:"oid"`
+		Type          string      `json:"type"`
+		UserOid       interface{} `json:"userOid"`
+		CoinType      string      `json:"coinType"`
+		CoinTypePair  string      `json:"coinTypePair"`
+		Direction     string      `json:"direction"`
+		Price         float64     `json:"price"`
+		DealAmount    float64     `json:"dealAmount"`
+		PendingAmount float64     `json:"pendingAmount"`
+		CreatedAt     int64       `json:"createdAt"`
+		UpdatedAt     int64       `json:"updatedAt"`
+	} `json:"BUY"`
 }
 
-type rawActiveOrders struct {
-	Comment string       `json:"_comment"`
-	Success bool         `json:"success"`
-	Code    string       `json:"code"`
-	Data    ActiveOrders `json:"data"`
+type rawActiveOrder struct {
+	Success   bool        `json:"success"`
+	Code      string      `json:"code"`
+	Msg       string      `json:"msg"`
+	Timestamp int64       `json:"timestamp"`
+	Data      ActiveOrder `json:"data"`
 }
